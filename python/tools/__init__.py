3d7702c1bddf8d04f396ff7585ab78e3a392a742
Oleksandr Frei <oleksandr.frei@gmail.com>
2016-10-07 14:55:41 -0700

    Auto-generate documentation for a toy python module

---
+++ python/tools/__init__.py
@@ -0,0 +1,1 @@
+from .goodness import Goodness
---
+++ python/tools/goodness.py
@@ -0,0 +1,14 @@
+class Goodness(object):
+    def __init__(self, value='Hello world!'):
+        """
+            Initialize Goodness object.
+            :param str value: text to output in do() method
+        """
+        self._value = value
+
+    def make_it(self):
+        """
+           Prints some text to the console.
+        """
+        print(self._value)
+        return True
